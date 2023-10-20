a3930e6ed24ee55f358ff50405e5bfe98ab1b68e
Daww <dav.saez@duouc.cl>
2023-10-20 17:00:23 -0300

    Saludo

--- src/app/services/auth.service.service.ts
+++ src/app/services/auth.service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { log, showAlertError, showToast } from '../model/message';
+import { showToast } from 'src/app/tools/message-routines';
 import { Usuario } from '../model/usuario';
 import { Storage } from '@ionic/storage-angular';
 import { DataBaseService } from './data-base.service';
@@ -28,14 +28,12 @@ export class AuthService {
   async login(correo: string, password: string) {
     await this.storage.get(this.keyUsuario).then(async (usuarioAutenticado) => {
       if (usuarioAutenticado) {
-        
         this.bd.actualizarSesionActiva(correo, 'S');
         this.storage.set(this.keyUsuario, usuarioAutenticado);
         this.usuarioAutenticado.next(usuarioAutenticado);
         this.router.navigate(['home/qr']);
       } else {
         await this.bd.validarUsuario(correo, password).then(async (usuario: Usuario | undefined) => {
-          
           if (usuario) {
             showToast(`¡Bienvenido(a) ${usuario.nombre} ${usuario.apellido}!`);
             this.bd.actualizarSesionActiva(correo, 'S');
@@ -52,25 +50,28 @@ export class AuthService {
   }
 
   async logout() {
-    try {
-      const usuario = await this.leerUsuarioAutenticado();
-  
+    this.leerUsuarioAutenticado().then((usuario) => {
       if (usuario) {
-        await this.storage.remove(this.keyUsuario);
-        this.usuarioAutenticado.next(null);
         showToast(`¡Hasta pronto ${usuario.nombre} ${usuario.apellido}!`);
         this.bd.actualizarSesionActiva(usuario.correo, 'N');
+        this.storage.remove(this.keyUsuario);
+        this.usuarioAutenticado.next(null);
+        this.router.navigate(['login']);
+      } else {
+        this.router.navigate(['login']);
       }
-  
-      this.router.navigate(['login']);
-    } catch (error) {
-      console.error('Error durante el logout:', error);
-      this.router.navigate(['login']);
-    }
+    })
   }
 
   async leerUsuarioAutenticado(): Promise<Usuario | undefined> {
-    return this.storage.get(this.keyUsuario).then(usuario => usuario as Usuario);
+    const usuario = await this.storage.get(this.keyUsuario).then(usuario => usuario as Usuario);
+    this.usuarioAutenticado.next(usuario);
+    return usuario;
+  }
+
+  setUsuarioAutenticado(usuario: Usuario) {
+    this.storage.set(this.keyUsuario, usuario);
+    this.usuarioAutenticado.next(usuario);
   }
 
 }
